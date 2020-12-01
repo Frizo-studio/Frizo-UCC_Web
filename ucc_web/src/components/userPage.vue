589ec1cfbf0b3ea70bd5cfc3100e873043bc49a3
EricHsueh88 <erichsueh880605@gmail.com>
2020-12-01 21:13:08 +0800

    finish userPage

--- ucc_web/src/components/userPage.vue
+++ ucc_web/src/components/userPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="userPage">
     <!-- useCenter為固定水平置中，merge進user群組時，可調整刪除 -->
+    <div class="userIntroductionBg"></div>
     <div class="useCenter">
       <div class="userHome">
         <div class="userIntroduction">
@@ -15,16 +16,27 @@
           <div class="divider"></div>
         </div>
         <div class="userActivity">
-          <div class="btnGroup">
-            <el-radio-group v-model="radio" @change="changeList">
-              <el-radio :label="1">
-                <b>活動</b>
-              </el-radio>
-              <el-radio :label="2">
-                <b>消息</b>
-              </el-radio>
-            </el-radio-group>
-          </div>
+          <el-tabs
+            class="switchTag"
+            v-model="activeName"
+            @tab-click="handleClick"
+          >
+            <el-tab-pane class="contentPage" label="活動" name="first"
+              ><keep-alive>
+                <component
+                  class="searchLists"
+                  :is="listPrint"
+                ></component> </keep-alive
+            ></el-tab-pane>
+            <el-tab-pane class="contentPage" label="消息" name="second"
+              ><keep-alive>
+                <component
+                  class="searchLists"
+                  :is="listPrint"
+                ></component> </keep-alive
+            ></el-tab-pane>
+          </el-tabs>
+
           <div class="activityContent"></div>
         </div>
       </div>
@@ -33,14 +45,31 @@
 </template>
 
 <script>
+import comprehensiveList from "@/components/searchResultListGroup/comprehensiveList";
+import activityList from "@/components/searchResultListGroup/activityList";
+
 export default {
+  components: {
+    comprehensiveList,
+    activityList,
+  },
   data() {
     return {
       userName: "roger",
-      description:
-        "簡介最長幾字簡介最長幾字簡介最長幾字簡介最長幾字簡介最長幾字簡介最長幾字簡介最長幾字簡介最長幾字簡介最長幾字簡介最長幾字",
+      description: "自我介紹",
       radio: 1,
+      activeName: "first",
+      listPrint: "comprehensiveList",
     };
+  },
+  methods: {
+    handleClick() {
+      if (this.listPrint == "comprehensiveList") {
+        this.listPrint = "activityList";
+      } else {
+        this.listPrint = "comprehensiveList";
+      }
+    },
   },
 
   name: "userPage",
@@ -48,6 +77,11 @@ export default {
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0;
+}
 .userPage {
   /* 高度之後再做微調 */
   height: 200vh;
@@ -66,21 +100,27 @@ export default {
   /* 高度之後再做微調 */
   height: 200vh;
   background-color: #ffffff;
-  border: 1px #cfcfcf solid;
 }
 
 .userIntroduction {
   position: relative;
-  width: 750px;
+  width: 100%;
   height: 400px;
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-template-rows: repeat(8, 1fr);
 }
-
+.userIntroductionBg {
+  position: absolute;
+  width: 100%;
+  /* background-color: #fff; */
+  height: 400px;
+}
 .userBackgroundPhoto {
   grid-column: 1/9;
   grid-row: 1/5;
+  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
   background-color: aqua;
 }
 
@@ -104,17 +144,19 @@ export default {
   font-size: 20px;
 }
 
-.divider {
-  position: relative;
-  top: 5px;
-  grid-column: 2/8;
-  grid-row: 8/9;
-  border-bottom: 1px #aaaaaa solid;
-}
-
 .userActivity {
   position: relative;
   height: 100vh;
-  top: 5px;
+  /* top: 5px; */
+  background-color: #fff;
+  padding: 5px 25px 0 25px;
+}
+.switchTag {
+  /* margin-top: 5px; */
+  background-color: #fff;
+}
+.contentPage {
+  /* background-color: rgb(165, 101, 42); */
+  height: 700px;
 }
 </style>
